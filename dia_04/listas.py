d4ae1aa763191d0aec23272557916f5b510815d2
Dhiego Silva <dhiego.tdk@gmail.com>
2026-03-13 17:05:24 -0300

    Commit 3° dia

---
+++ dia_04/listas.py
@@ -0,0 +1,37 @@
+#%%
+
+idades = [28 , 35 , 42 , 50, 26, 48, 68]
+print(idades)
+
+#%%
+
+Dhiego = ["Dhiego", 21, "cabeludo", "namorando", "gato"]
+print(Dhiego)
+ 
+ #%%
+
+type(Dhiego)
+
+#%%
+#idade
+print(Dhiego[1])
+
+#estado civil
+print(Dhiego[3])
+
+#%%
+
+idades = [28 , 35 , 42 , 50, 26, 48, 68, 17]
+
+print("Soma das idades:", sum(idades))
+
+print("Quantidade de idades:", len(idades))
+
+print("Média das idades:", sum(idades) / len(idades))
+
+print("Idade mais nova:", min(idades))
+
+print("Idade mais velha:", max(idades))
+
+#%%
+dir()
